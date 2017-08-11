63a8a39ea33bde2bf53074a4528a356d86f740e2
ZHAOSHUANGSHUANG <zhaoshuangshuang@xinguangnet.com>
2017-08-11 18:19:12 +0800

    添加PC端业务页面模板

---
+++ src/page/vueTpl.vue
@@ -0,0 +1,20 @@
+<template>
+    <!-- 主框架路由页面入口 -->
+    <div class="content-wrap">
+         
+     </div>
+</template>
+<script>
+
+export default {
+    data() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+}
+</script>
